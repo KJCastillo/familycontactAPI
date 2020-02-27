a2f5ef0845f0a0588110b33f9c57c416653dfe7b
Kevin Castillo <kevincastillo318@gmail.com>
2020-02-27 01:05:27 +0000

    started project

--- src/models.py
+++ src/models.py
@@ -1,17 +1,52 @@
-from flask_sqlalchemy import SQLAlchemy
+class Family:
 
-db = SQLAlchemy()
+    def __init__(self, last_name):
+        self.last_name = last_name
+        # example list of members
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+        # read-only: Use this method to generate random members ID's when adding members into the list
+    def _generateId(self):
+        return random.randint(0, 99999999) //import random 
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def add_member(self, member):
+        ## you have to implement this method
+        ## append the member to the list of _members
+        pass
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def delete_member(self, id):
+        ## you have to implement this method
+        ## loop the list and delete the member with the given id
+        pass
+
+    def update_member(self, id, member):
+        ## you have to implement this method
+        ## loop the list and replace the memeber with the given id
+        pass
+
+    def get_member(self, id):
+        ## you have to implement this method
+        ## loop all the members and return the one with the given id
+        pass
+
+    def get_all_members(self):
+        return self._members
